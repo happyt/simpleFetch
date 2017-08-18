40d696ff61cc876471e12e8a217217d801ebc977
Ian Morton <ian.morton@sky.uk>
2017-08-18 14:41:09 +0100

    xml2js version - working

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 ## Strip player countries
 
-This is a short app to pull golfers and their countries from the feed, so that we can create a dictionary for use in the studio app.
+This is a short app to pull golfers and their countries from a feed, so that we can create a dictionary for use in the studio app.
 
-It reads the file from PGA with fetch.
+It reads the file with fetch.
 
-It outputs a file which contains the XML lines needed for the dictionary. These will then be cut and pasted into the production file. I was using the xml-js library. This didn't produce the XML output that I was expecting, so I left it as a cut & paste job. May try with a different library later.
+start1.js
+
+It outputs a file which contains the XML lines needed for the dictionary. These will then be cut and pasted into the production file. I thought I'd try using the xml-js library. This didn't produce the XML output that I was expecting, so I left it as a cut & paste job. 
+
+start.js
+Trying xml2js lib - sorted it out properly with this to the XML array etc. USE THIS VERSION. 
 
 It is basically going through and checking whether the player has a code for the tour, together with a non-blank country code. I have set it up for 'R' which is the normal PGA Tour. The codes can be in different places in the XML, for some reason and sometimes they are included on more than one tour.  Currently, if there is an array of tours, I only look at the first entry.
 
--- start.js
+++ start.js
@@ -1,8 +1,10 @@
 var config = require('./config.js');
 var fetchUrl = require("fetch").fetchUrl;
-var fs = require("fs");
+var fs = require('fs'),
+    xml2js = require('xml2js');
 
-var parser = require("xml-js");
+var options = { trim: true, explicitArray : false };
+var parser = new xml2js.Parser(options);
 
 // source file is iso-8859-15 but it is converted to utf-8 automatically
 fetchUrl(config.feeds[0].url,
@@ -12,78 +14,91 @@ fetchUrl(config.feeds[0].url,
         } else {
             //         if (!quiet) console.log(body);
             // write to file
-            fs.writeFile('c:/golf/USAplayers2017.xml', body, function (err) {
-                if (err) return console.log(err);
-                console.log('Y');
-            });
-            var jsonObj = parser.xml2js(body, { compact: true, spaces: 4 });
-            // fs.writeFile('c:/golf/USAplayers2017.json', jsonObj, function (err) {
+            // fs.writeFile('c:/golf/USAplayers2017.xml', body, function (err) {
             //     if (err) return console.log(err);
-            //     console.log('Z');
+            //     console.log('Y');
             // });
+
+            // test        var xmlIn = "<root>Hello xml2js!</root>";
             var count = 0;
             var xmlList = ""
-            var theList = { "entries": [] }
-            console.log("decl = ", jsonObj._declaration)
-            console.log("plr count = ", jsonObj.plrs.plr.length)
-            for (var p in jsonObj.plrs.plr) {
-                //           console.log("country = ", jsonObj.plrs.plr[p].name.Country)
+            var theList = {"entries": { "lookupEntry" : []} }
 
-                if (playerOnTour(jsonObj.plrs.plr[p], "R")) {
-                    var entry = jsonObj.plrs.plr[p].name;
-                    var newEntry = { "dictionaryentry": {} }
+            parser.parseString(body, function (err, jsonObj) {
 
-                    newEntry.dictionaryentry.longname = entry.last._text + "," + entry.first._text
-                    newEntry.dictionaryentry.entryname = entry.Country._text
-                    if (count < 14) console.log("New: ", newEntry);  // just to see a few lines
+                // fs.writeFile('./xml2jsOutput.json', JSON.stringify(jsonObj), function (err) {
+                //     if (err) return console.log(err);
+                //     console.log('Full file written');
+                // });
 
-                    // write out to a file
-                    // tried to convert json to xml, but lib didn't give me what I expected, so will just cut and past result
-            // with ID             xmlList += "<dictionaryentry><num>" + jsonObj.plrs.plr[p]._attributes.plrNum + "</num><longname>" + entry.last._text + "," + entry.first._text + "</longname><entryname>" + entry.Country._text + "</entryname></dictionaryentry>\n"
-                    xmlList += "<dictionaryentry><longname>" + entry.last._text + "," + entry.first._text + "</longname><entryname>" + entry.Country._text + "</entryname></dictionaryentry>\n"
-                    theList.entries.push(newEntry)
-                    count++;
+                console.log("plr count = ", jsonObj.plrs.plr.length)
+                for (var p in jsonObj.plrs.plr) {
+          //          console.log("output = ", JSON.stringify(jsonObj.plrs.plr[p]))
+                    if (playerOnTour(jsonObj.plrs.plr[p], "R")) {
+                        var entry = jsonObj.plrs.plr[p].name;
+                        var newEntry = {}
+
+                        newEntry.longname = entry.last + "," + entry.first
+                        newEntry.entryname = entry.Country
+                        if (count < 14) console.log("New: ", newEntry);  // just to see a few lines
+
+                        // write out to a file
+                        // tried to convert json to xml, but lib didn't give me what I expected, so will just cut and past result
+                        // with ID             xmlList += "<dictionaryentry><num>" + jsonObj.plrs.plr[p]._attributes.plrNum + "</num><longname>" + entry.last._text + "," + entry.first._text + "</longname><entryname>" + entry.Country._text + "</entryname></dictionaryentry>\n"
+                        xmlList += "<dictionaryentry><longname>" + entry.last + "," + entry.first + "</longname><entryname>" + entry.Country + "</entryname></dictionaryentry>\n"
+                        theList.entries.lookupEntry.push(newEntry)
+                        count++;
+                    }
+               //     count++;
+               //     if (count > 14) break;
                 }
- // debug              if (count > 14) break;
-            }
+            })
         }
         //     console.log(JSON.stringify(jsonObj));
         console.log("total = ", count);
-        fs.writeFile('c:/golf/countrynames4.xml', xmlList, function (err) {
+        fs.writeFile('c:/golf/countrynamesList.xml', xmlList, function (err) {
             if (err) return console.log(err);
             console.log('OK');
         });
+
         //        console.log("the list = ", theList)
-        // testing
-        theList = { overall: { abc: { _text: "abc" }, bbc: [{ aaa: { _text: "aaa" } }, { aaa: { _text: "bbb" } }, { aaa: { _text: "ccc" } }] } }
-        var options = { ignoreComment: true, spaces: 4, compact: true };
-        var xmlresult = parser.js2xml(theList, options);
-        //         console.log("result = ", xmlresult);          
+
+        // // testing convert obj to XML
+        // theList = { overall: { abc: { name: "abc" }, bbc: [{ aaa: { name: "aaa" } }, { aaa: { name: "bbb" } }, { aaa: { name: "ccc" } }] } }
+        var builder = new xml2js.Builder({"rootName": "lookupDictionary"});
+        var xmlresult = builder.buildObject(theList);
+        fs.writeFile('c:/golf/countrynames5.xml', xmlresult, function (err) {
+            if (err) return console.log(err);
+            console.log('OK');
+        });
+        // console.log("result = ", xmlresult);
     });
 
+    // use the console.log to see that get some of each type
 function playerOnTour(p, code) {
     var result = false;
     if (p.tour !== undefined) {
         // we have a tour block
-        if (p.tour._attributes === undefined) {
+        if (p.tour.$ === undefined) {
             // tour is an array in this case, look at each entry...
             //just try printing first entry...
-            if (p.name.Country._text !== undefined && p.tour[0]._attributes.tourCodeUC === code) {
+            if (p.name.Country !== "" && p.tour[0].$.tourCodeUC === code) {
                 result = true;
-    //            console.log("tour array");
+      //                  console.log("tour array");
             }
         } else {
-            if (p.tour._attributes.tourCodeUC === code) {
-                if (p.name.Country._text !== undefined) {
+            if (p.tour.$.tourCodeUC === code) {
+                if (p.name.Country !== "") {
                     result = true;
+      //                  console.log("tour w attrs");
                 }
             }
         }
     } else {
         // no tour defined - use the main attributes
-        if (p.name.Country._text !== undefined && p._attributes.primaryTourUC === code) {
+        if (p.name.Country !== "" && p.$.primaryTourUC === code) {
             result = true;
- //           console.log("no tour");
+      //                 console.log("no tour");
         }
     }
 
